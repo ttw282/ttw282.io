3d6f0c79794a2bfee57d7089c14d2a4073b18ee4
Tony Wei <tony.wei282@gmail.com>
2019-05-03 19:53:48 -0700

    fix

--- custom/gascalculator.js
+++ custom/gascalculator.js
@@ -86,3 +86,25 @@ function changeLang() {
         $('#calcbtn').html('划算吗？');
     }
 }
+
+function getUrlParameter(sParam) {
+    var sPageURL = window.location.search.substring(1),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+        }
+    }
+};
+
+$(document).ready(function () {
+    var lang = getUrlParameter('lang');
+    if (lang == "zh") {
+        $('#changelangbtn').click();
+    }
+});
